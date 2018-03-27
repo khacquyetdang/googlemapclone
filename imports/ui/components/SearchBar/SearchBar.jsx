1be026c82e35cb3e10a9f660d2ea55e939839ba2
mrdang <khacquyet.dang.pro@gmail.com>
2018-03-27 14:24:42 +0200

    search when any of field is changed

--- imports/ui/components/SearchBar/SearchBar.jsx
+++ imports/ui/components/SearchBar/SearchBar.jsx
@@ -8,17 +8,28 @@ export default class SearchBar extends Component {
     {
         super();
         this.state = {
-            term: null
+            term: null,
+            location: null
         }
     }
-
+    onSearch = () => {
+        if (this.state.term && this.state.location) {
+            this
+                .props
+                .onSearchEnterKey(this.state.location, this.state.term);
+        }
+    }
+    onTermKeyPress = (event) => {
+        event.which = event.which || event.keyCode;
+        if (event.which === 13) {
+            this.onSearch();
+        }
+    }
     onLocationKeyPress = (event) => {
         //event.preventDefault();
         event.which = event.which || event.keyCode;
         if (event.which === 13) {
-            this
-                .props
-                .onSearchEnterKey(event.target.value, this.state.term);
+            this.onSearch();
         }
     }
     render() {
@@ -28,6 +39,7 @@ export default class SearchBar extends Component {
                     <Col s={12}>
                         <Input
                             s={12}
+                            onKeyPress={this.onTermKeyPress}
                             onChange={(e, value) => this.setState({term: value})}
                             label='What are you looking for (type Restaurant, Bar...) ?'/>
                     </Col>
@@ -36,11 +48,10 @@ export default class SearchBar extends Component {
                 <Row>
                     <Col s={12}>
                         <Input
-                        s={12}
-                            id="searchInput"
+                            s={12}
                             type="text"
-                            placeholder="Enter your destination"
-                            className="validate"
+                            label="Enter your destination"
+                            onChange={(e, value) => this.setState({location: value})}
                             onKeyPress={this.onLocationKeyPress}></Input>
                         {this.props.error && <div className="helper-text error" data-error="wrong">{this.props.error}</div>
 }
